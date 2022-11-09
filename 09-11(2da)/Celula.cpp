edb3b8f31bccfc9a27f6f0aa6e60b031eb0be91a
maledesm <116035546+maledesm@users.noreply.github.com>
2022-11-09 03:28:56 -0300

    Add files via upload

---
+++ 09-11(2da)/Celula.cpp
@@ -0,0 +1,51 @@
+#include "Celula.h"
+
+Celula::Celula(){
+    this->gen1 = new Gen(255);
+    this->gen2 = new Gen(255);
+    this->gen3 = new Gen(255);
+}
+Celula::Celula(int gene1, int gene2, int gene3){
+    this->gen1 = new Gen(255,gene1);
+    this->gen2 = new Gen(255,gene2);
+    this->gen3 = new Gen(255,gene3);
+}
+void Celula::setGen1(int cargaGenetica){
+    this->gen1->setCargaGentica(cargaGenetica);
+}
+
+void Celula::setGen2(int cargaGenetica){
+    this->gen2->setCargaGentica(cargaGenetica);
+}
+
+void Celula::setGen3(int cargaGenetica){
+    this->gen3->setCargaGentica(cargaGenetica);
+}
+
+void Celula::setGens(int cargaGenetica1, int cargaGenetica2, int cargaGenetica3){
+    this->setGen1(cargaGenetica1);
+    this->setGen2(cargaGenetica2);
+    this->setGen3(cargaGenetica3);
+}
+
+int Celula::getCargaGeneticaGen1(){
+    return this->getGen1()->getCargaGentica();
+}
+
+int Celula::getCargaGeneticaGen2(){
+    return this->getGen2()->getCargaGentica();
+}
+
+int Celula::getCargaGeneticaGen3(){
+    return this->getGen3()->getCargaGentica();
+}
+
+Gen * Celula::getGen1(){
+    return (this->gen1);
+}
+Gen * Celula::getGen2(){
+    return (this->gen2);
+}
+Gen * Celula::getGen3(){
+    return (this->gen3);
+}
---
+++ 09-11(2da)/JuegoDeLaVida.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Tablero.h"
+#include "Lista.h"
+using namespace std;
+
+
+
+int main(){
+	Tablero tablerin(5,5,5);
+	tablerin.imprimirTablero();
+	return 0;
+}
