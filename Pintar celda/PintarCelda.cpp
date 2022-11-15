543824ecc4589e22b0f7bb69b246277ed356cac0
MaxiPrantera <68716532+MaxiPrantera@users.noreply.github.com>
2022-11-15 20:51:53 -0300

    Update PintarCelda.cpp

--- Pintar celda/PintarCelda.cpp
+++ Pintar celda/PintarCelda.cpp
@@ -1,29 +1,77 @@
 #include "Comportamiento.h"
 #include "EasyBMP.h"
 
-void visualizador::dibujar celda(int ALTO, int ANCHO, comportamientoCelda comportamiento){
+void visualizador::pintarComportamiento(int ALTO, int ANCHO, comportamientoCelda comportamiento){
     switch(comportamiento){
         case(envenenada){
-            int rojo = 19;
-            int verde = 21;
-            int azul = 170;
+            int rojo = 245;
+            int verde = 0;
+            int azul = 0;
             int alpha = 0;
             break;
         }
         case(portal){
-            int rojo = 19;
-            int verde = 21;
-            int azul = 140;
+            int rojo = 33;
+            int verde = 227;
+            int azul = 243;
             int alpha = 0;
             break;
         }
+        case(Radioactiva){
+            int rojo = 33;
+            int verde = 243;
+            int azul = 52;
+            int alpha = 0;
+            break;
+        }
+        case(intercambiadora){
+            int rojo = 113;
+            int verde = 24;
+            int azul = 149;
+            int alpha = 0;
+            break;
+        }
+        case(contaminada){
+            int rojo = 241;
+            int verde = 234;
+            int azul = 129;
+            int alpha = 0;
+            break;
+        }
+	    for(int x = 0; x<640; x++){
+		    for(int y = 420; y<480; y++){
+				
+                AnImage(x,y)->Red = 50;
+                AnImage(x,y)->Green = 50;
+                AnImage(x,y)->Blue = 192;
+                AnImage(x,y)->Alpha = 0;
+		    }
+	    }
+        for(int x = 580; x<640; x++){
+            for(int y = 0; y<480; y++){
 
-        for(int x=inicio_x;x<(inicio_x + PIXELES_POR_CELDA);x++){
-            for(int y=inicio_y;y<(inicio_y + PIXELES_POR_CELDA);y++){
-                this->capa(x,y)->Red = rojo;
-                this->capa(x,y)->Green = verde;
-                this->capa(x,y)->Azul = blue;
-                this->capa(x,y)->Alpha = alpha;
+                AnImage(x,y)->Red = 50;
+                AnImage(x,y)->Green = 50;
+                AnImage(x,y)->Blue = 192;
+                AnImage(x,y)->Alpha = 0;
+            }
+        }
+        for(int x = 0; x<640; x++){
+            for(int y = 0; y<60; y++){
+
+                AnImage(x,y)->Red = 50;
+                AnImage(x,y)->Green = 50;
+                AnImage(x,y)->Blue = 192;
+                AnImage(x,y)->Alpha = 0;
+            }
+        }
+        for(int x = 0; x<60; x++){
+            for(int y = 0; y<480; y++){
+
+                AnImage(x,y)->Red = 50;
+                AnImage(x,y)->Green = 50;
+                AnImage(x,y)->Blue = 192;
+                AnImage(x,y)->Alpha = 0;
             }
         }
     }
