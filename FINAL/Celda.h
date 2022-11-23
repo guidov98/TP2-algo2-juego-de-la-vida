f570d35a762f83b85c86dae3d1cb8a8ec21243ef
maledesm <116035546+maledesm@users.noreply.github.com>
2022-11-23 17:36:10 -0300

    Add files via upload

---
+++ FINAL/Celda.h
@@ -0,0 +1,147 @@
+#ifndef CELDA_H_
+#define CELDA_H_
+
+#ifndef NULL
+#define NULL 0
+#endif /* NULL */
+
+#include "Celula.h"
+#include <stdlib.h>
+#include <time.h>
+
+enum comportamientoCelda{
+	Inerte,				//Celda sin comportamientos extra
+	//Color: Blanca/Negra
+	Contaminada,		//No deja crecer una celula
+	//Color: Amarilla
+	Portal,				//Duplica el nacimiento de una celula en otra
+	//Color: Celeste
+	Radioactiva,		//Afecta a 1 o varios genes al heredarse
+	//Color: Verde
+	Envenenada,  		//Mata un Gen
+	//Color: Roja
+	Intercambiadora,	//Intercambia Gen1 por Gen2, Gen2 por Gen3, Gen3 por Gen1
+	//Color: Violeta
+	CantidadComportamientos
+};
+
+enum tipoDeTransmicion{
+	InvertirProgenitores,
+	PromediosCuadrados,
+	Minimos
+};
+
+class Celda{
+private:
+	Celula * celula;
+	comportamientoCelda comportamiento;
+	tipoDeTransmicion transmicion;
+	Celda**** vecinos;
+	
+public:
+
+/*	Pre:  -
+	Post:  Crea una celda con comportamiento Inerte, su celula muerta y sus vecinos como NULL*/
+	Celda();
+
+/*	Pre:  -
+	Post: Crea una celda con comportamiento state, una nueva celula y sus vecinos como NULL*/
+	Celda(comportamientoCelda state);
+
+/*	Pre: cell no puede ser NULL 
+	Post: Crea una celda con comportamiento Inerte, su celula igual a cell, y sus vecinos como NULL*/
+	Celda(Celula * cell);
+
+/*	Pre: cell no puede ser NULL
+	Post: Crea una celda con comportamiento state, su celula igual a cell, y sus vecinos como NULL*/
+	Celda(comportamientoCelda state, Celula * cell);
+
+
+/*	Pre: -
+	Post: Devuelve un puntero a la Celula de la Celda*/
+	Celula * getCelula();
+
+/*	Pre: -
+	Post: Devuelve el tipoDeTransmicion de la Celda*/
+	tipoDeTransmicion getTransmicion();
+
+/*	Pre: -
+	Post: Devuelve el comportamientoCelda de la Celda*/
+	comportamientoCelda getComportamiento();
+
+/*	Pre: -
+	Post: Devuelve los nuevos genes de la Celda*/
+	int * getNewGenes();
+
+/*	Pre: -
+	Post: Devuelve los punteros de las Celdas vecinas */
+	Celda**** getVecinos();
+
+
+/*	Pre: -
+	Post: Setea la celula de la celda igual a cell */
+	void setCelula(Celula * cell);
+
+/*	Pre: -
+	Post: Setea el tipoDeTransmicion de la celda igual a tipoTransmicion */
+	void setTransmicion(tipoDeTransmicion tipoTransmicion);
+
+/*	Pre: -
+	Post: Setea el comportamientoCelda la celda igual a nuevoComportamiento*/
+	void setComportamiento(comportamientoCelda nuevoComportamiento);
+
+/*	Pre: -
+	Post: Setea el comportamientoCelda de la celda a un comportamiento aleatorio*/
+	void setComportamientoRandom();
+
+/*	Pre: -
+	Post: Setea los vecinos de la celda como el vectorVecinos*/
+	void setVecinos(Celda **** vectorVecinos);
+
+
+/*	Pre: -
+	Post: Devuelve si la celula de la celda esta viva o no*/
+	bool hayCelulaViva();
+
+/*	Pre: 	el valor de cada gen debe estar entre 0 y 255
+	Post: 	Si la celula de la Celda esta muerta, la setea como viva con los genes indicados
+			Si ya esta viva, devuelve un mensaje de error indicandolo*/
+	void nacerCelula(int gen1, int gen2, int gen3);
+
+/*	Pre: -
+	Post: Si la celula de la Celda esta viva, la setea a NULL, y si esta muerta devuelve un mensaje de error indicandolo*/
+	void matarCelula();
+
+/*	Pre: -
+	Post: Devuelve la cantidad de Celulas vecinas vivas*/
+	int contarVecinos();
+
+/*	Pre: -
+	Post: Setea a la Celda igual a celdaFuente*/
+	void copiarCelda(Celda * celdaFuente);
+
+/*	Pre: La Celda debe estar inicializada
+	Post: Elimina la Celda*/
+	~Celda();
+
+private:
+
+/*	Pre: -
+	Post: Setea todos los vecinos a NULL*/
+	void inicializarVecinos();
+
+/*	Pre: -
+	Post: Devuelve el promedio invertido de los genes Celdas vecinas */
+	int * getPromediosInvertidosVecinos();
+
+/*	Pre: -
+	Post: Devuelve el promedio cuadratico de los genes Celdas vecinas */
+	int * getPromedioCuadraticoVecinos();
+/*	Pre: -
+	Post: Devuelve el minimo de los genes Celdas vecinas */
+	int * getMinimoVecinos();
+};
+
+
+
+#endif /* CELDA_H_ */
---
+++ FINAL/JuegoDeLaVida.cpp
@@ -0,0 +1,241 @@
+#include "JuegoDeLaVida.h"
+#include "Funciones.h"
+
+
+
+JuegoDeLaVida::JuegoDeLaVida(Configuraciones configuracion){
+
+	switch(configuracion){
+        case(Configuracion1):
+            iniciarConfiguracionUno();
+            break;
+        case(Configuracion2):
+            iniciarConfiguracionDos();
+            break;
+        case(Configuracion3):
+            iniciarConfiguracionTres();
+            break;
+        case(ConfiguracionManual):
+            iniciarConfiguracionManual();
+            break;
+    }
+
+	this->muertesTotales = 0;
+	this->turnoActual = 1;
+    this->graficos = new Visualizador(this->tablero->getN(), this->tablero->getM());
+}
+
+JuegoDeLaVida::~JuegoDeLaVida(){
+    delete this->tablero;
+    delete this->graficos;
+}
+
+void JuegoDeLaVida::iniciarConfiguracionUno(){
+	this->tablero = new Tablero(5,5,5, InvertirProgenitores);
+	this->transmicion = InvertirProgenitores;
+	this->x1 = 2;
+	this->x2 = 5;
+	this->x3 = MAXIMOS_VECINOS;
+
+	//x=5,y=3,z=7, gen1=x*25, gen2=y*25
+	for (int i = 0; i < 5; i++){
+		this->tablero->revivirCelulaPos(i,i,1,i*50,i*50,1*50);
+    }
+	this->nacimientosTotales = 5;
+	
+}
+
+void JuegoDeLaVida::iniciarConfiguracionDos(){
+	this->tablero = new Tablero(5,5,5, PromediosCuadrados);
+	this->transmicion = PromediosCuadrados;
+	this->x1 = 3;
+	this->x2 = 2;
+	this->x3 = 4;
+
+	this->tablero->revivirCelulaPos(1,3,1,250,250,0);
+	this->tablero->revivirCelulaPos(2,3,1,250,210,40);
+	this->tablero->revivirCelulaPos(3,3,1,250,170,80);
+	this->tablero->revivirCelulaPos(3,2,1,250,130,120);
+	this->tablero->revivirCelulaPos(2,1,1,250,90,160);
+	this->nacimientosTotales = 5;
+}
+
+void JuegoDeLaVida::iniciarConfiguracionTres(){
+	this->tablero = new Tablero(5,7,4, Minimos);
+	this->transmicion = Minimos;
+	this->x1 = 4;
+	this->x2 = 8;
+	this->x3 = 13;
+	int nacimientos=0;
+	for (int i=0; i<5; i++){
+		for (int j=0; j<2; j++){
+			nacimientos +=this->tablero->revivirCelulaPos(i,	0,	j*3,	50,	100,25);
+			nacimientos +=this->tablero->revivirCelulaPos(i,	6,	j*3,	200,5,	150);
+			nacimientos +=this->tablero->revivirCelulaPos(0,	i+1,j*3,	5,	5,	150);
+			nacimientos +=this->tablero->revivirCelulaPos(4,	i+1,j*3,	5,	5,	5);
+		}
+	}
+	for (int i=1; i<3; i++){
+		nacimientos +=this->tablero->revivirCelulaPos(0,0,i,0,0,0);
+		nacimientos +=this->tablero->revivirCelulaPos(4,0,i,0,0,0);
+		nacimientos +=this->tablero->revivirCelulaPos(0,6,i,0,0,0);
+		nacimientos +=this->tablero->revivirCelulaPos(4,6,i,0,0,0);
+	}
+	this->nacimientosTotales = nacimientos;
+	
+}
+
+void JuegoDeLaVida::iniciarConfiguracionManual(){
+    tipoDeTransmicion tipoTransmicion;
+	int n, m, l;
+	int x1user, x2user, x3user;
+
+	std::cout << "Ingrese los valores del tamanio del tablero (entre 3 y 50):" << std::endl;
+	std::cout << "N: ";
+	n = Funciones::pedirInput(3,50);
+	std::cout << "M: ";
+	m = Funciones::pedirInput(3,50);
+	std::cout << "L: ";
+	l = Funciones::pedirInput(3,50);
+
+	std::cout << "Que tipo de transmicion quiere?" << std::endl;
+	std::cout << "1- Promedios" << std::endl;
+	std::cout << "2- Promedios Cuadraticos" << std::endl;
+	std::cout << "3- Minimos" << std::endl;
+	tipoTransmicion = (tipoDeTransmicion)Funciones::pedirInput(1,3);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Ingrese la cantidad de celulas que debe haber alrededor" << std::endl;
+	std::cout << "de una celula muerta para que nazca (0-26): ";
+	x1user = Funciones::pedirInput(MINIMOS_VECINOS, MAXIMOS_VECINOS);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Ingrese la cantidad MINIMA de celulas que debe haber" << std::endl;
+	std::cout << "alrededor de una celula viva para que siga viva (0-26): ";
+	x2user = Funciones::pedirInput(MINIMOS_VECINOS, MAXIMOS_VECINOS);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Ingrese la cantidad MAXIMA de celulas que debe haber" << std::endl;
+	std::cout << "alrededor de una celula viva para que siga viva (" << x2 << "-26): ";
+	x3user = Funciones::pedirInput(x2, MAXIMOS_VECINOS);
+	std::cout << std::endl << std::endl;
+    
+    this->x1 = x1user;
+    this->x2 = x2user;
+    this->x3 = x3user;
+
+    this->tablero = new Tablero(n, m, l,tipoTransmicion);
+    
+    elegirCelulasIniciales();
+}
+
+int JuegoDeLaVida::getX1(){
+    return this->x1;
+}
+int JuegoDeLaVida::getX2(){
+    return this->x2;
+}
+int JuegoDeLaVida::getX3(){
+    return this->x3;
+}
+
+int JuegoDeLaVida::getCantidadCelulasVivas(){
+	return this->tablero->getCantidadCelulas();
+}
+
+int JuegoDeLaVida::getTurnoActual(){
+	return this->turnoActual;
+}
+
+//tablero->getCeldaPos(1,1,1)->nacerCelula();
+void JuegoDeLaVida::elegirCelulasIniciales(){
+	int cantidadCelulas = 0;
+    std::cout << "Ingrese la cantidad de celulas iniciales: ";
+    std::cin >> cantidadCelulas;
+    std::cout << std::endl;
+
+    int celulaPosicion[3];
+	int celulaGen[3];
+
+    for (int i = 0; i < cantidadCelulas; i++){
+        std::cout << "Ingrese la coordenada x de la celula a nacer: ";
+        celulaPosicion[0] = Funciones::pedirInput(1,this->tablero->getL());
+		std::cout << std::endl;
+            
+        std::cout << "Ingrese la coordenada y de la celula a nacer: ";
+        celulaPosicion[1] = Funciones::pedirInput(1,this->tablero->getL());
+        std::cout << std::endl;
+
+        std::cout << "Ingrese la coordenada z de la celula a nacer: ";
+        celulaPosicion[2] = Funciones::pedirInput(1,this->tablero->getN());
+        std::cout << std::endl;
+
+
+        std::cout << "Ingrese el gen1 de la celula a nacer: ";
+        celulaGen[0] = Funciones::pedirInput(0,255);
+		std::cout << std::endl;
+            
+        std::cout << "Ingrese el gen2 de la celula a nacer: ";
+        celulaGen[1] = Funciones::pedirInput(0,255);
+        std::cout << std::endl;
+
+        std::cout << "Ingrese el gen3 de la celula a nacer: ";
+        celulaGen[2] = Funciones::pedirInput(0,255);
+        std::cout << std::endl;
+		//revivirCelulaPos(int x, int y, int z, int gen1, int gen2, int gen3)
+		this->tablero->revivirCelulaPos(celulaPosicion[0]-1, celulaPosicion[1]-1,celulaPosicion[2]-1,celulaGen[0],celulaGen[1],celulaGen[2]);
+    }
+	this->nacimientosTotales = cantidadCelulas;
+	
+}
+
+
+void JuegoDeLaVida::actualizarJuego(){
+	this->tablero->actualizarTablero(this->x1, this->x2, this->x3);
+}
+
+void JuegoDeLaVida::mostrarTablero(){
+	int altura = this->tablero->getL();
+
+	for(int i=0; i<altura; i++){
+		this->graficos->dibujarCapa(this->tablero, i);
+		this->graficos->exportarImagen(i);
+	}
+
+}
+
+int JuegoDeLaVida::getNacimientosTotales(){
+	return this->nacimientosTotales;
+}
+
+int JuegoDeLaVida::getMuertesTotales(){
+	return this->muertesTotales;
+}
+
+void JuegoDeLaVida::setNacimientosTotales(int cantidad){
+	this->nacimientosTotales = cantidad;
+}
+
+void JuegoDeLaVida::setMuertesTotales(int cantidad){
+	this->muertesTotales = cantidad;
+}
+
+void JuegoDeLaVida::ejecutarTurno(){
+	int celulasNacidas = 0;
+    int celulasFallecidas = 0;
+
+	int celulasPrevias = this->tablero->getCantidadCelulas();
+	celulasNacidas = this->tablero->actualizarTablero(this->getX1(), this->getX2(), this->getX3());
+	int celulasActuales = this->tablero->getCantidadCelulas();
+	celulasFallecidas= celulasNacidas - (celulasActuales - celulasPrevias); 
+	
+	this->nacimientosTotales += celulasNacidas;
+	this->muertesTotales += celulasFallecidas;
+	this->turnoActual++;
+	float promedioNacimientos = (float)(this->nacimientosTotales) / (float)(this->turnoActual);
+	float promedioMuertes = (float)(this->muertesTotales) / (float)(this->turnoActual);
+	this->mostrarTablero();
+	bool estaCongelado = (celulasNacidas==0 && celulasFallecidas==0);
+	Funciones::mostrarInformacion(this->turnoActual, celulasNacidas, celulasFallecidas, promedioNacimientos, promedioMuertes, estaCongelado);
+}
+
